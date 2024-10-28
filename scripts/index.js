3c60b6c851d7ecd56564bcb17020d1dd0b9cc9fb
Alex-Ovch <aakirlasheva@mai.education>
2024-10-28 23:28:23 +0300

    test 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function cardDelete(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, cardDelete);
+  placesList.appendChild(cardElement);
+});
