5820b3cc59d65cd71f29b3a255419a69dc34c2b9
Alex-Ovch <aakirlasheva@mai.education>
2024-10-29 23:09:54 +0300

    review -2

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,17 @@
 const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 function createCard(cardData, deleteCallback) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
 
-  cardElement.querySelector(".card__image").src = cardData.link;
-  cardElement.querySelector(".card__image").alt = cardData.name;
-  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", () => {
@@ -21,13 +23,13 @@ function createCard(cardData, deleteCallback) {
 }
 
 // @todo: Функция удаления карточки
-function cardDelete(cardElement) {
+function deleteCard(cardElement) {
   cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((cardData) => {
-  const cardElement = createCard(cardData, cardDelete);
-  placesList.appendChild(cardElement);
+  const cardElement = createCard(cardData, deleteCard);
+  cardsContainer.appendChild(cardElement);
 });
